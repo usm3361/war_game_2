51974f03d1c76079d552ae69a97ef599818b5f81
Uri Miller <usm3361@gmail.com>
2025-12-07 18:22:35 +0200

    added function createPlayer

--- utils/players.js
+++ utils/players.js
@@ -1,2 +1,10 @@
-import input from "analiza-sync";
 
+function create_player(name='AI') {
+    const player = {
+        name,
+        hand: [],
+        wonPile:[]
+    }
+    return player
+}
+export default create_player
